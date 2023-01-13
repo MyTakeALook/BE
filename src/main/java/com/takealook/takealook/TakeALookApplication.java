847b0c9798386c527be26e8e4f15edc35443c713
hwi3721 <lsh3721@gmail.com>
2023-01-13 15:31:32 +0900

    first commit

---
+++ src/main/java/com/takealook/takealook/TakeALookApplication.java
@@ -0,0 +1,13 @@
+package com.takealook.takealook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakeALookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TakeALookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/takealook/takealook/TakeALookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.takealook.takealook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TakeALookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
