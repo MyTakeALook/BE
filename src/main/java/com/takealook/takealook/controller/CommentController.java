75edd19ec27a33f3d541e1d1e32cb610d828bbb0
kwon woo <dirn0568@naver.com>
2023-01-19 05:11:23 +0900

    constrouctor

--- src/main/java/com/takealook/takealook/controller/CommentController.java
+++ src/main/java/com/takealook/takealook/controller/CommentController.java
@@ -1,5 +1,6 @@
 package com.takealook.takealook.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.takealook.takealook.dto.CommentRequestDto;
 import com.takealook.takealook.dto.CommentResponseDto;
 import com.takealook.takealook.dto.ResponseDto;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,16 +22,41 @@ public class CommentController {
     @PostMapping("/board/{boardId}") // @RequestBody
     public CommentResponseDto commentCreate(@PathVariable Long boardId,
                                             @RequestBody CommentRequestDto commentRequestDto,
-                                            @AuthenticationPrincipal UserDetailsImpl userDetailsImpl,
-                                            HttpServletRequest request) throws IOException {
+                                            @AuthenticationPrincipal UserDetailsImpl userDetailsImpl) {
 
         System.out.println("1111111111111111111111111111111111");
         System.out.println(boardId);
-        System.out.println(request.getReader().readLine());
+        //System.out.println(request.getReader().lines().getClass());
         System.out.println(commentRequestDto.getComment()); // null
-        System.out.println(request.getReader().readLine());
+        //System.out.println(request.getReader().readLine());
         //System.out.println(userDetailsImpl.getUser().getUsername());
         System.out.println("1111111111111111111111111111111111");
+
+//        public class ObjectMapperEx {
+//            public static void main(String[] args) {
+//                ObjectMapper objectMapper = new ObjectMapper();
+//
+//                // Java Object ->  JSON
+//                Person person = new Person("zooneon", 25, "seoul");
+//                try {
+//                    objectMapper.writeValue(new File("src/person.json"), person);
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        }
+//
+//        private void getSummary (String searchDate) {
+//            try {
+//                ResponseObject responseObject = resourceClient.getForResponseObject();
+//
+//                ObjectMapper om = new ObjectMapper();
+//                Summary summary = om.convertValue(responseObject.getData(), new TypeReference<Summary>() {});
+//
+//            } catch (Exception e) {
+//                log.error(e.getMessage());
+//            }
+//        }
 
         return commentService.createComment(boardId, commentRequestDto, userDetailsImpl);
     }
--- src/main/java/com/takealook/takealook/dto/CommentRequestDto.java
+++ src/main/java/com/takealook/takealook/dto/CommentRequestDto.java
@@ -1,11 +1,15 @@
 package com.takealook.takealook.dto;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class CommentRequestDto { //
     private String comment;
 
+    public CommentRequestDto() {} // 빈생성자를 넣어줘야 받을 수 있음
     public CommentRequestDto(String comment) { // form데이터 받기 위해서 생성자 추가해줌
         this.comment = comment;
     }
